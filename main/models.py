b744184647ecd5f5a403bbab7b68a4e727080de6
waldan-r <waldan.rafid@ui.ac.id>
2025-09-23 00:58:10 +0700

    Tugas 4: Auth, Session, Cookies

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
@@ -19,6 +20,7 @@ class Product(models.Model):
     stock = models.IntegerField(default=0)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     is_featured = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,35 +1,92 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from main.models import Product
 from main.forms import ProductForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import datetime
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
+    filter_type = request.GET.get('filter', 'all')
 
-    product_list = Product.objects.all()
+    if filter_type == 'all':
+        product_list = Product.objects.all()
+    else:
+        product_list = Product.objects.filter(user=request.user)
+    
     context = {
         'namaToko' : 'Bolahraga',
         'nama' : 'Waldan Rafid',
         'kelas' : 'PBP F',
-        'product_list' : product_list
+        'product_list' : product_list,
+        'last_login' : request.COOKIES.get('last_login', 'Never'),
     }
     return render(request, "main.html", context)
 
+
+# FUNGSI REGISTER DAN LOGIN #
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+        
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form =  AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse('main:show_main'))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+
+    context = {'form':form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+#FUNGSI UTAMA PROGRAM
 def add_product(request):
     form = ProductForm(request.POST or None)
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product_entry = form.save(commit=False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
     
     context = {'form': form}
     return render(request, "add_product.html", context)
 
+@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     context = {'product':product}
     return render(request, "product_detail.html", context)
 
+
+
+# Data Delivery (XML/JSON) #
 def show_xml(request):
     product_list = Product.objects.all()
     xml_data = serializers.serialize("xml", product_list)
